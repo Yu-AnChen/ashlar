23afeb2eb2f30c82d6a7a4ee1bb179cb85170b2e
Unknown <atwood12@gmail.com>
2020-02-11 12:14:47 -0500

    Hard-code cycle offset

--- ashlar/reg.py
+++ ashlar/reg.py
@@ -739,6 +739,7 @@ class LayerAligner(object):
         self.cycle_offset = thumbnail.calculate_cycle_offset(
             self.reference_aligner.reader, self.reader
         )
+        self.cycle_offset = [-10, -10]
         self.corrected_nominal_positions = self.metadata.positions + self.cycle_offset
         reference_positions = self.reference_aligner.metadata.positions
         dist = scipy.spatial.distance.cdist(reference_positions,
