c329a489605bf3778916e0f86f5bb7ab904a128b
Yu-An Chen <atwood12@gmail.com>
2020-08-04 12:28:27 -0400

    Add script for processing and debugging the tonsil image

--- ashlar/svs.py
+++ ashlar/svs.py
@@ -129,8 +129,9 @@ import pathlib
 import sklearn
 
 settings = {
-    'svs_dir': 'H:/2019NOV-TNP_PILOT/TNP SARDANA images (mIHC)/75682/Raw images',
+    'svs_dir': r'H:\2019NOV-TNP_PILOT\TNP SARDANA images (mIHC)\75684\processed\raw',
     'ref_slide_pattern': '*HEM*',
+    'out_dir': r'H:\2019NOV-TNP_PILOT\TNP SARDANA images (mIHC)\75684\processed\registration'
 }
 
 svs_paths = sorted(pathlib.Path(settings['svs_dir']).glob('*.svs'))
@@ -154,20 +155,84 @@ c1e.origin = c1e.positions.min(axis=0)
 c1e.centers = c1e.positions + c1e.metadata.size / 2
 
 m_shape = c1r.slide.dimensions[::-1]
-out_name = 'ashlar/' + c1r.path.name.replace(
-    '.svs', '-c{channel}-t250-o0.02-m30.tif'
-)
+out_dir = pathlib.Path(settings['out_dir'])
+if not out_dir.exists():
+    out_dir.mkdir(parents=True)
+out_name = '{}-c{}-t250-o0.02-m30.tif'.format(c1r.path.stem, '{channel}')
 
-reg.Mosaic(c1e, m_shape, out_name, verbose=True, channels=[0, 5], combined=True, tile_size=1024).run()
+reg.Mosaic(
+    c1e, m_shape, str(out_dir / out_name), verbose=True, 
+    channels=[5], combined=True, tile_size=1024
+).run()
 
-for i in svs_paths[7:]:
+for i in svs_paths[:]:
     print('Processing', i.name)
     c2r = SvsReader(str(i), (250, 250), 0.02, 1.)
     c21l = reg.LayerAligner(c2r, c1e, verbose=True, max_shift=30)
     c21l.run()
-    out_name_layer = 'ashlar/' + c2r.path.name.replace(
-        '.svs', '-c{channel}-t250-o0.02-m30.tif'
-    )   
-    reg.Mosaic(c21l, m_shape, out_name_layer, verbose=True, channels=[0, 4], combined=True, tile_size=1024).run()
+    out_name_layer = '{}-c{}-t250-o0.02-m30.tif'.format(c2r.path.stem, '{channel}')
+  
+    reg.Mosaic(
+        c21l, m_shape, str(out_dir / out_name_layer), verbose=True,
+        channels=[4], combined=True, tile_size=1024
+    ).run()
 
-reg.build_pyramid('75682-t250-o0.02-m30-norm.ome.tif', int(2 * (len(svs_paths) + 1)), c1e.mosaic_shape, c1r.metadata.pixel_dtype, c1r.metadata.pixel_size, 1024, verbose=True)
+# reg.build_pyramid('75682-t250-o0.02-m30-norm.ome.tif', int(2 * (len(svs_paths) + 1)), c1e.mosaic_shape, c1r.metadata.pixel_dtype, c1r.metadata.pixel_size, 1024, verbose=True)
+
+# Debugging
+import pathlib
+import sklearn
+
+settings = {
+    'svs_dir': r'H:\2019NOV-TNP_PILOT\TNP SARDANA images (mIHC)\75684\processed\raw',
+    'ref_slide_pattern': '*HEM*',
+    'out_dir': r'H:\2019NOV-TNP_PILOT\TNP SARDANA images (mIHC)\75684\processed\registration'
+}
+
+svs_paths = sorted(pathlib.Path(settings['svs_dir']).glob('*.svs'))
+
+# Identify the ref slide to for the shape of the mosaic
+ref_slide = None
+for svs in svs_paths:
+    if svs.match(settings['ref_slide_pattern']):
+        print('Reference file found {}'.format(svs.name))
+        ref_slide = svs
+        svs_paths.remove(svs)
+        break
+
+c1r = SvsReader(str(ref_slide), (1250, 1250), 0.02, 1.)
+c1e = reg.EdgeAligner(c1r, verbose=True, max_shift=50)
+c1e.positions = c1e.metadata.positions
+
+c1e.lr = sklearn.linear_model.LinearRegression()
+c1e.lr.fit(c1e.metadata.positions, c1e.positions)
+c1e.origin = c1e.positions.min(axis=0)
+c1e.centers = c1e.positions + c1e.metadata.size / 2
+
+
+def layeralign(filepath):
+    c1r = SvsReader(str(ref_slide), (1250, 1250), 0.02, 1.)
+    c1e = reg.EdgeAligner(c1r, verbose=True, max_shift=50)
+    c1e.positions = c1e.metadata.positions
+
+    c1e.lr = sklearn.linear_model.LinearRegression()
+    c1e.lr.fit(c1e.metadata.positions, c1e.positions)
+    c1e.origin = c1e.positions.min(axis=0)
+    c1e.centers = c1e.positions + c1e.metadata.size / 2
+
+    m_shape = c1r.slide.dimensions[::-1]
+    print('Processing', filepath.name)
+    c2r = SvsReader(str(filepath), (1250, 1250), 0.02, 1.)
+    c21l = reg.LayerAligner(c2r, c1e, verbose=True, max_shift=100)
+    c21l.run()
+    c21l.mosaic_shape = m_shape
+    c21l.reader = None
+    c21l.reference_aligner = None
+    return c21l
+
+from joblib import Parallel, delayed
+las = Parallel(n_jobs=8, verbose=1)(delayed(layeralign)(i) for i in svs_paths[:8])
+
+for i, l in zip(svs_paths[:8], las):
+    l.reader = SvsReader(str(i), (1250, 1250), 0.02, 1.)
+    l.reference_aligner = c1e
