Fix image-mask size mismatch due to rotation center crop

ashlar/reg.py
@@ -1182,7 +1182,7 @@ class Mosaic(object):
                 sys.stdout.flush()
 
             mask = np.ones(
-                self.aligner.metadata.tile_size(si),
+                self.aligner.metadata.size,
                 dtype=self.aligner.metadata.pixel_dtype,
             )
             if do_mask:
@@ -1190,7 +1190,7 @@ class Mosaic(object):
             if not np.any(mask):
                 continue
             mosaic_slice = utils.calculate_mosaic_position(
-                position, self.aligner.metadata.tile_size(si), self.shape
+                position, self.aligner.metadata.size, self.shape
             )["mosaic"]
             if mosaic_slice is not None:
                 img = self.aligner.reader.read(c=channel, series=si)

ashlar/tissue_mask.py
@@ -73,8 +73,8 @@ def make_reader_mask(reader, thumbnail_px_size=20, **kwargs):
 
     tile_mask = zarr.group()
     for ii, pp in enumerate(tqdm.tqdm(positions, desc="Making tissue mask")):
-        h, w = metadata.tile_size(ii)
-        out_shape = np.ceil(factor * metadata.tile_size(ii)).astype("int")
+        h, w = metadata.size
+        out_shape = np.ceil(factor * metadata.size).astype("int")
         tform = Affine(translation=pp[::-1])
         tmask = skimage.transform.warp(mask, tform, output_shape=out_shape, order=0)
         tile_mask[str(ii)] = skimage.transform.rescale(tmask, 1 / factor, order=0)[